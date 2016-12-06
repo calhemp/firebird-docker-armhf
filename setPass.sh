b8168bf5ef29e465fa9f1f30029db541b664f6c1
calhemp <calhemp@gmail.com>
2016-12-06 19:07:07 +0100

    Update setPass.sh

--- setPass.sh
+++ setPass.sh
@@ -9,3 +9,16 @@ ISC_PASSWD=masterkey
 # Your password can be changed to a more suitable one using the
 # /usr/local/firebird/bin/gsec utility.
 EOL
+
+
+#echo "Setting SYSDBA's password..."
+#cd /usr/local/firebird/
+#IBASE_PASSWORD=`grep -Po '(?<=ISC_PASSWD=)[^\s]*' ./SYSDBA.password`
+#echo "Previous password was ${IBASE_PASSWORD}"
+#./bin/fbguard -onetime -daemon
+#sleep 10
+#./bin/gsec -user sysdba -pass $IBASE_PASSWORD -mo sysdba -pw masterkey
+
+#CURRENT_SYSDBA_PASSWORD=`grep -oP 'ISC_PASSWD=\K.*' /usr/local/firebird/SYSDBA.password`
+#/usr/local/firebird/bin/gsec -user SYSDBA -password `echo $CURRENT_SYSDBA_PASSWORD` -modify SYSDBA -pw `echo $SYSDBA_PASSWORD`
+#/usr/local/firebird/bin/fbguard
